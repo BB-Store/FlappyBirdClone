476f1b630fa7c13f4dd163a5e934adbc416dbdda
BB-Store <bb-store@gmx.de>
2021-01-21 00:29:30 +0100

    Initial commit

---
+++ desktop/src/de/tutorial/flappybird/clone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.tutorial.flappybird.clone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.tutorial.flappybird.clone.Mainactivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mainactivity(), config);
+	}
+}

64d36624586de8bf3e01f96ce44b3d1956ebf226
BB-Store <bb-store@gmx.de>
2021-03-07 00:35:29 +0100

    Part 15 - Admob Bannerwerbung + Gradleanpassungen

--- android/src/de/tutorial/flappybird/clone/AndroidLauncher.java
+++ android/src/de/tutorial/flappybird/clone/AndroidLauncher.java
@@ -1,16 +1,58 @@
 package de.tutorial.flappybird.clone;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import de.tutorial.flappybird.clone.Mainactivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class AndroidLauncher extends AndroidApplication {
+
+	private AdView adView;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Mainactivity(), config);
+
+		StartGameWithBanner(config);
+	}
+
+	private void StartGameWithBanner(AndroidApplicationConfiguration config) {
+		RelativeLayout layout = new RelativeLayout(this);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+		layout.setLayoutParams(params);
+
+		View gameView = initializeForView(new Mainactivity(), config);
+
+		RelativeLayout.LayoutParams gameViewParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		gameViewParam.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+		gameViewParam.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+
+		gameView.setLayoutParams(gameViewParam);
+		layout.addView(gameView);
+
+		adView = new AdView(this);
+		adView.setAdSize(AdSize.BANNER);
+		adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+		adView.setVisibility(View.VISIBLE);
+
+		AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
+		adView.loadAd(adRequestBuilder.build());
+
+		RelativeLayout.LayoutParams topParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		topParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,  RelativeLayout.TRUE);
+		topParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+		layout.addView(adView, topParams);
+		adView.setBackgroundColor(Color.TRANSPARENT);
+
+		setContentView(layout);
+
+
 	}
 }
